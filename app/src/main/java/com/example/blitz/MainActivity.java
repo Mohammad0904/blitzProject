0a77bc4dde5651c641302df1687f42a5ddd5a355
Mohammad Sadman Shabab Hossain <sadman31@my.yorku.ca>
2022-03-28 23:59:48 -0400

    Connected pages

--- app/src/main/java/com/example/blitz/MainActivity.java
+++ app/src/main/java/com/example/blitz/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.blitz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button beginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        beginButton = (Button) findViewById(R.id.beginButton);
+        beginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                        openactivity_main2();
+
+        }});
+
+
+    }
+
+    public void openactivity_main2() {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
