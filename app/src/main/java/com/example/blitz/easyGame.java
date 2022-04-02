a2b3bc64955230346970dff0861946ab61ed6193
Mohammad Sadman Shabab Hossain <sadman31@my.yorku.ca>
2022-04-02 02:08:27 -0400

    easygame done

--- app/src/main/java/com/example/blitz/easyGame.java
+++ app/src/main/java/com/example/blitz/easyGame.java
@@ -5,26 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class easyGame extends AppCompatActivity {
     int answer;
+    double easeScore = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easy_game);
-        generateRandomNumber();
     }
 
-    public void generateRandomNumber() {
+    /** public void generateRandomNumber() {
         Random easyRandom = new Random();
         this.answer = easyRandom.nextInt(10) + 1;
-    }
+    } **/
 
     public void easyButtonClicked(View v) {
         EditText easyGuess = (EditText) findViewById(R.id.easyGuess);
         String eS = easyGuess.getText().toString();
+
+        Double eD = Compare.parseDouble(eS);
+        int eI = Compare.convertToInt(eD);
+        int eR = Compare.generateRandomNumber();
+        boolean easyCom = Compare.comparison(eI, eR);
+
+        if (easyCom == true) {
+            easeScore++;
+            String display = Compare.format(easeScore);
+            ((TextView) findViewById(R.id.easyScore)).setText(display);
+        }
+        else {
+            String display = Compare.format(easeScore);
+            ((TextView) findViewById(R.id.easyScore)).setText(display);
+        }
     }
 }
